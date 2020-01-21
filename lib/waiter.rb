faf5e80200cfa2db586cbf535d8e29b00ecde160
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-21 22:19:56 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -15,7 +15,8 @@ class Waiter
      end 
    end 
     def new_meal(customer,total,tip=0)
-    Meal.new(self,customer,total,tip)
+     Meal.new(self,customer,total,tip)
+    end 
    def best_tipper 
      best_tipper_meal = meals.max do |meal_a,meal_b|
        meal_a.tip <=> meal_b.tip 
